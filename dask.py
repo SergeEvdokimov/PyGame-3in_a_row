5a5fe5d0abc3f55879a098bebd0ca9d3d3653d11
Elina <elinaglushanok@gmail.com>
2023-12-30 04:36:43 +0300

    Update dask: load_image from main

--- dask.py
+++ dask.py
@@ -3,20 +3,6 @@ import os
 from random import randint
 from main import load_image
 
-def load_image(name, color_key=None):
-    fullname = os.path.join('data', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error as message:
-        print('Не удаётся загрузить:', name)
-        raise SystemExit(message)
-    image = image.convert_alpha()
-    if color_key is not None:
-        if color_key == -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    return image
-
 
 class Board:
     def __init__(self, width, height, left=0, top=50):
