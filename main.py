e455455998b9ea5a95d452474480b35859b1459e
Elina <elinaglushanok@gmail.com>
2024-01-28 02:12:39 +0300

    Serge movenents

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 import pygame
-import sys
 from load_image import load_image
 from dask import Board
+import sys
+from PyQt5.QtWidgets import QApplication
+import sqlite3
 
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow
+
+con = sqlite3.connect('Results.sqlite')
+cur = con.cursor()
 size = (700, 700)
+
+class Enter(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Enter.ui', self)
+        self.setWindowTitle("Вход")
+        self.EnterButton.clicked.connect(self.enter)
+
+    # Вход
+    def check_name(self, name):
+        return len(cur.execute(f"""SELECT * FROM result
+                        WHERE Name = '{name}'""").fetchall())
+
+    def enter(self):
+        company_is_registered = self.check_name(self.Name.text())
+        if not company_is_registered:
+            cur.execute(f"""INSERT INTO result(Name, num_of_move) 
+                                    VALUES('{self.Name.text()}', '{0}')""")
+        self.close()
+
 
 def intro(screen, start_background_image):
     intro_text = ["Выберите размер поля:", "7 на 7", "10 на 10", "12 на 12"]
@@ -16,7 +43,6 @@ def intro(screen, start_background_image):
     for line in intro_text:
         text = font.render(line, 1, (255, 66, 103))
         rect = text.get_rect()
-        #text_y += 5
         text_x = 240
         if line == "7 на 7":
             text_x = 320
@@ -40,17 +66,23 @@ def intro(screen, start_background_image):
                         return levels.index(i)
         pygame.display.flip()
 
+
 def make_current_board(board):
     line_for_del = board.del_line()
     cnt = 0
-    if not line_for_del is None:
+    if line_for_del is not None:
         cnt = len(line_for_del)
-    while not line_for_del is None:
+    while line_for_del is not None:
         board.delete(line_for_del)
         line_for_del = board.del_line()
     return cnt
 
+
 def main():
+    app = QApplication(sys.argv)
+    ex = Enter()
+    #sys.exit(app.exec_())
+
     global size
     pygame.init()
     screen = pygame.display.set_mode(size)
@@ -59,6 +91,8 @@ def main():
     start_background.image = start_background_image
     start_background.rect = start_background.image.get_rect()
     pygame.display.set_caption('3 в ряд')
+
+    ex.show()
 
     lvl_num = intro(screen, start_background_image)
     size = 420, 470
@@ -71,34 +105,37 @@ def main():
     else:
         board = Board(12, screen2)
     screen2.fill('white')
-    cnt = 0
+    cnt, res = 0, 100
+    step_cnt = 0
     cnt_fon = pygame.font.Font(None, 36)
-    score = cnt_fon.render('Scores:', True, (255, 66, 103))
+    score = cnt_fon.render('Счет:', True, (255, 66, 103))
+    need_score = cnt_fon.render('Цель: 100', True, (255, 66, 103))
     counter = cnt_fon.render(f'{cnt}', True, (180, 0, 0))
     screen2.blit(score, (10, 10))
     screen2.blit(counter, (100, 10))
+    screen2.blit(need_score, (200, 10))
     board.render()
     pygame.display.flip()
     make_current_board(board)
     pygame.display.flip()
     running = True
-    while running:
+    while running and cnt < res:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 board.get_click(event.pos)
-                c = cnt # временная переменная, чтобы понять, прибавилось ли очков
-                cnt += make_current_board(board)
-                if c != cnt: # если очки прибавились, то обновляем счетчик
-                    pygame.draw.rect(screen2, 'white', (100, 10, 28, 25),)
+                new_board = make_current_board(board)
+                if new_board:  # если очки прибавились, то обновляем счетчик
+                    cnt += new_board
+                    step_cnt += 1
+                    pygame.draw.rect(screen2, 'white', (100, 10, 100, 25))
                     counter = cnt_fon.render(f'{cnt}', True, (180, 0, 0))
                     screen2.blit(counter, (100, 10))
         pygame.display.flip()
-        #TODO: сделать переход на заключительный экран в случае окончания игры
+    #  TODO: таблица результатов
     pygame.quit()
 
 
 if __name__ == '__main__':
     main()
-
